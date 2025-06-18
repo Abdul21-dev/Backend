2cced1095ba8f8209f774996f3c77219a34f7d69
Abdul21-dev <abdulrahman21062004@gmail.com>
2025-06-18 20:58:57 +0530

    ...

--- EJSDIR/Frontend/script.js
+++ EJSDIR/Frontend/script.js
@@ -28,29 +28,29 @@
 
 // Using concept of inheritance
 
-class Person{
-    constructor(name, age){
-        this.name = name;
-        this.age = age;
-    }
-    talk(){
-        console.log(`Hello my name is ${this.name}`)
-    }
-}
+// class Person{
+//     constructor(name, age){
+//         this.name = name;
+//         this.age = age;
+//     }
+//     talk(){
+//         console.log(`Hello my name is ${this.name}`)
+//     }
+// }
 
-class Student extends Person{
-    constructor(name, age, marks){
-        super(name, age);
-        this.marks = marks;
-    }
-}
+// class Student extends Person{
+//     constructor(name, age, marks){
+//         super(name, age);
+//         this.marks = marks;
+//     }
+// }
 
-class Teacher extends Person{
-    constructor(name, age, subject){
-        super(name, age);
-        this.subject = subject;
-    }
-}
+// class Teacher extends Person{
+//     constructor(name, age, subject){
+//         super(name, age);
+//         this.subject = subject;
+//     }
+// }
 
-let std1 = new Student("Abdul", 20, 95);
-let t1   = new Teacher("Ranitesh", 45, "BEE");
+// let std1 = new Student("Abdul", 20, 95);
+// let t1   = new Teacher("Ranitesh", 45, "BEE");
